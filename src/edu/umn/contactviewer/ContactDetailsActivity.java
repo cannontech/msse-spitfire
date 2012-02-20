98dfc4b2046f9bf5ca310e60288ac38a0915e700
jeff.lemmerman@gmail.com <jeff.lemmerman@gmail.com@172621b8-09b4-d279-b058-66819d0f2f19>
2012-02-20 05:27:27 +0000

    Added toolbar to Details, Title, and right button visible with text

--- src/edu/umn/contactviewer/ContactDetailsActivity.java
+++ src/edu/umn/contactviewer/ContactDetailsActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.*;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 
 /**
  * presents details for a single contact item
@@ -14,7 +15,12 @@ public class ContactDetailsActivity extends Activity implements OnClickListener 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        setContentView(R.layout.details);        
+        setContentView(R.layout.details);
+
+        ToolbarConfig tb = new ToolbarConfig(this, "Contact Details");
+        Button btn = tb.getToolbarRightButton();
+        btn.setVisibility(1);
+        btn.setText("Edit");
     }
 
 	public void onClick(View v) {
