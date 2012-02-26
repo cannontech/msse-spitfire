45f63ad0074e0bccd9ba589ffbca7075f5ba9290
todd.becicka@gmail.com <todd.becicka@gmail.com@172621b8-09b4-d279-b058-66819d0f2f19>
2012-02-26 17:29:02 +0000

    add item long press listener to contact list for delete

--- gen/edu/umn/contactviewer/R.java
+++ gen/edu/umn/contactviewer/R.java
@@ -29,27 +29,28 @@ public final class R {
         public static final int window_background=0x7f020006;
     }
     public static final class id {
-        public static final int about_button=0x7f070010;
-        public static final int company=0x7f070003;
-        public static final int contacts_button=0x7f07000f;
-        public static final int item_icon=0x7f07000c;
-        public static final int item_name=0x7f07000a;
-        public static final int item_phone=0x7f07000d;
-        public static final int item_title=0x7f07000b;
-        public static final int labelEmail=0x7f070006;
-        public static final int labelPhone=0x7f070004;
-        public static final int labelTwitter=0x7f070008;
-        public static final int logo=0x7f07000e;
-        public static final int name=0x7f070001;
-        public static final int test_image=0x7f070000;
-        public static final int title=0x7f070002;
-        public static final int toolbar=0x7f070011;
-        public static final int toolbar_back_button=0x7f070012;
-        public static final int toolbar_right_button=0x7f070014;
-        public static final int toolbar_title=0x7f070013;
-        public static final int valueEmail=0x7f070007;
-        public static final int valuePhone=0x7f070005;
-        public static final int valueTwitter=0x7f070009;
+        public static final int about_button=0x7f080010;
+        public static final int company=0x7f080003;
+        public static final int contacts_button=0x7f08000f;
+        public static final int delete=0x7f080015;
+        public static final int item_icon=0x7f08000c;
+        public static final int item_name=0x7f08000a;
+        public static final int item_phone=0x7f08000d;
+        public static final int item_title=0x7f08000b;
+        public static final int labelEmail=0x7f080006;
+        public static final int labelPhone=0x7f080004;
+        public static final int labelTwitter=0x7f080008;
+        public static final int logo=0x7f08000e;
+        public static final int name=0x7f080001;
+        public static final int test_image=0x7f080000;
+        public static final int title=0x7f080002;
+        public static final int toolbar=0x7f080011;
+        public static final int toolbar_back_button=0x7f080012;
+        public static final int toolbar_right_button=0x7f080014;
+        public static final int toolbar_title=0x7f080013;
+        public static final int valueEmail=0x7f080007;
+        public static final int valuePhone=0x7f080005;
+        public static final int valueTwitter=0x7f080009;
     }
     public static final class layout {
         public static final int details=0x7f030000;
@@ -58,11 +59,15 @@ public final class R {
         public static final int main=0x7f030003;
         public static final int toolbar=0x7f030004;
     }
+    public static final class menu {
+        public static final int list_context_menu=0x7f070000;
+    }
     public static final class string {
         public static final int about=0x7f050003;
         public static final int app_name=0x7f050001;
         public static final int back=0x7f050004;
         public static final int contacts=0x7f050002;
+        public static final int delete=0x7f050005;
         public static final int hello_world=0x7f050000;
     }
     public static final class style {
