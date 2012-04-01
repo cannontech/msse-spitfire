cbf7d64ba3775a91b3e225d420506195db811e50
todd.becicka@gmail.com <todd.becicka@gmail.com@172621b8-09b4-d279-b058-66819d0f2f19>
2012-04-01 19:05:13 +0000

    initial import of moviemon client

---
+++ trunk/MovieMon/src/com/moviemon/data/Cast.java
@@ -0,0 +1,16 @@
+package com.moviemon.data;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Cast {
+
+    @SerializedName("Name")
+    private String name;
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+}
